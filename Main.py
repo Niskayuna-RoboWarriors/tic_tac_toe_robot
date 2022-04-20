c8d0adc5c1dd883f08458a89a6c1366af0026236
jSdCool <37940266+jSdCool@users.noreply.github.com>
2022-04-20 13:37:45 -0400

    added configurations for input button pins and indicator LED pins added a function that sets the color of the LED

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 #import RPi.GPIO as GPIO #this will be used on the actual device
 import gpio as GPIO# fake raspberry PI io library for development on not raspberry pi comment this out for any test/builds on a real PI
 import ServoController
+import AI
 GPIO.setmode(GPIO.BCM)#set the pin numbering mode
 #numbers of the pins the servos are connected to
 armServo1Pin=1#theese need to be set
@@ -34,3 +35,36 @@ armServo3Positions=[0,0,0,0,0,0,0,0,0]
 armServo4Positions=[0,0,0,0,0,0,0,0,0]
 armServo5Positions=[0,0,0,0,0,0,0,0,0]
 baseServoPositions=[0,0,0,0,0,0,0,0,0]
+#configure buttons and LED pins
+goButtonPin=1
+difficultyButtonPin=1
+GPIO.setUp(goButtonPin,GPIO.input)
+GPIO.setUp(difficultyButtonPin,GPIO.input)
+redPin=1
+greenPin=1
+bluePin=1
+GPIO.setUp(redPin,GPIO.output)
+GPIO.setUp(greenPin,GPIO.output)
+GPIO.setUp(bluePin,GPIO.output)
+
+def setLED():#sets the color of the LED based on the current difficulty
+    if AI.difficulty == 0:#green
+        GPIO.output(redPin, GPIO.LOW)
+        GPIO.output(greenPin, GPIO.HIGH)
+        GPIO.output(bluePin, GPIO.LOW)
+    if AI.difficulty==1:#light blue
+        GPIO.output(redPin,GPIO.LOW)
+        GPIO.output(greenPin, GPIO.HIGH)
+        GPIO.output(bluePin, GPIO.HIGH)
+    if AI.difficulty==2:#dark blue
+        GPIO.output(redPin,GPIO.LOW)
+        GPIO.output(greenPin, GPIO.LOW)
+        GPIO.output(bluePin, GPIO.HIGH)
+    if AI.difficulty==3:#purpleish white
+        GPIO.output(redPin,GPIO.HIGH)
+        GPIO.output(greenPin, GPIO.LOW)
+        GPIO.output(bluePin, GPIO.HIGH)
+    if AI.difficulty==4:#red
+        GPIO.output(redPin,GPIO.HIGH)
+        GPIO.output(greenPin, GPIO.LOW)
+        GPIO.output(bluePin, GPIO.LOW)
