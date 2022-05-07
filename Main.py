720f5aef8b75d018c741f09aebb669e3c2665e5d
jSdCool <37940266+jSdCool@users.noreply.github.com>
2022-05-07 10:20:18 -0400

    made the code compatible with the new controllers we are using in theory

--- Main.py
+++ Main.py
@@ -1,114 +1,90 @@
 #this is the main file to run at the start of the program
-import RPi.GPIO as GPIO #this will be used on the actual device
-#import gpio as GPIO# fake raspberry PI io library for development on not raspberry pi comment this out for any test/builds on a real PI
-from gpiozero import Servo
+import RPi.GPIO as GPIO #library for interacting with the pins on the pi
 import AI
 import time
 import analogReader
+from adafruit_servokit import ServoKit #library for interacting with the servo controller
 import GUI
 GUI.init()#start the GUI
 GPIO.setmode(GPIO.BCM)#set the pin numbering mode
 #numbers of the pins the servos are connected to
-armServo1Pin=1#theese need to be set
-armServo2Pin=2
-armServo3Pin=3
-armServo4Pin=4
-armServo5Pin=5
-handServoPin=6
-baseServoPin=7
-baseReader=analogReader.AnalogRead(8,9)
-#initilise the servo controllers
-armServo1=Servo(armServo1Pin)
-armServo2=Servo(armServo2Pin)
-armServo3=Servo(armServo3Pin)
-armServo4=Servo(armServo4Pin)
-armServo5=Servo(armServo5Pin)
-handServo=Servo(handServoPin)
-baseServo=Servo(baseServoPin)
+baseReader=analogReader.AnalogRead(7,8)
+#initilise the servo controller
+servos = ServoKit(channels=16)
+armServo1=1
+armServo2=2
+armServo3=3
+armServo4=4
+armServo5=5
+handServo=6
 #servo positions for each tile
-#                   1 2 3 4 5 6 7 8 9
-armServo1Positions=[0,0,0,0,0,0,0,0,0]#theese need to be set
-armServo2Positions=[0,0,0,0,0,0,0,0,0]
-armServo3Positions=[0,0,0,0,0,0,0,0,0]
-armServo4Positions=[0,0,0,0,0,0,0,0,0]#ser vo values range from -1 to 1
-armServo5Positions=[0,0,0,0,0,0,0,0,0]
-baseServoPositions=[0,0,0,0,0,0,0,0,0]
-initialBasePosition=3
-#configure buttons and LED pins
-goButtonPin=10
-difficultyButtonPin=11
-GPIO.setup(goButtonPin,GPIO.IN)
-GPIO.setup(difficultyButtonPin,GPIO.IN)
-redPin=12
-greenPin=13
-bluePin=14
-GPIO.setup(redPin,GPIO.OUT)
-GPIO.setup(greenPin,GPIO.OUT)
-GPIO.setup(bluePin,GPIO.OUT)
-
-def setLED():#sets the color of the LED based on the current difficulty
-    if AI.difficulty == 0:#green
-        GPIO.output(redPin, GPIO.LOW)
-        GPIO.output(greenPin, GPIO.HIGH)
-        GPIO.output(bluePin, GPIO.LOW)
-    if AI.difficulty==1:#light blue
-        GPIO.output(redPin,GPIO.LOW)
-        GPIO.output(greenPin, GPIO.HIGH)
-        GPIO.output(bluePin, GPIO.HIGH)
-    if AI.difficulty==2:#dark blue
-        GPIO.output(redPin,GPIO.LOW)
-        GPIO.output(greenPin, GPIO.LOW)
-        GPIO.output(bluePin, GPIO.HIGH)
-    if AI.difficulty==3:#purpleish white
-        GPIO.output(redPin,GPIO.HIGH)
-        GPIO.output(greenPin, GPIO.LOW)
-        GPIO.output(bluePin, GPIO.HIGH)
-    if AI.difficulty==4:#red
-        GPIO.output(redPin,GPIO.HIGH)
-        GPIO.output(greenPin, GPIO.LOW)
-        GPIO.output(bluePin, GPIO.LOW)
-
+#                   1 2 3 4 5 6 7 8 9 R
+armServo1Positions=[0,0,0,0,0,0,0,0,0,0]#theese need to be set
+armServo2Positions=[0,0,0,0,0,0,0,0,0,0]
+armServo3Positions=[0,0,0,0,0,0,0,0,0,0]
+armServo4Positions=[0,0,0,0,0,0,0,0,0,0]#ser vo values range from -1 to 1
+armServo5Positions=[0,0,0,0,0,0,0,0,0,0]
+baseServoPositions=[0,0,0,0,0,0,0,0,0,3]
+#set servos to their default positions
+servos.servo[armServo1].angle=armServo1Positions[-1]
+servos.servo[armServo2].angle=armServo2Positions[-1]
+servos.servo[armServo3].angle=armServo3Positions[-1]
+servos.servo[armServo4].angle=armServo4Positions[-1]
+servos.servo[armServo5].angle=armServo5Positions[-1]
+#configure the pins for the base servo
+baseServoSleepPin=14
+baseServoDirectionPin=15
+baseServoStepPin=18
+GPIO.setup(baseServoSleepPin, GPIO.OUT)#configure pins
+GPIO.setup(baseServoDirectionPin,GPIO.OUT)
+GPIO.setup(baseServoStepPin,GPIO.OUT)
+GPIO.output(baseServoSleepPin,False)
+stepPWM=GPIO.PWM(baseServoStepPin,100)#change the number here to change the frequency in HZ
+stepPWM.start(50)#start the PWM with a duty cycle of 50
+#configure outher buttons
+goButtonPin=23
+GPIO.setup(goButtonPin,GPIO.OUT)
 def setBasePos(val):
+    GPIO.output(baseServoSleepPin,True)#start the base motor
     curVal=baseReader.read()
-    while curVal<val-2 or curVal > val:
+    while curVal<val-2 or curVal > val+2:#set the direction of the motor
         if curVal > val:
-            baseServo.value=-0.5
+            GPIO.output(baseServoDirectionPin,True)
         if curVal < val:
-            baseServo.value=0.5
+            GPIO.output(baseServoDirectionPin,False)
         curVal=baseReader.read()
+    GPIO.output(baseServoSleepPin,False)#stop the base motor
 
 #continous execution stars here
-setLED()
 
 print("statring")
 while True:#forever
     GUI.updateScreen()
-    if GPIO.input(difficultyButtonPin)==1:#if the difficulty button is pressed
-        while GPIO.input(difficultyButtonPin)==1:
-            2+2#wait until the button is released
-        AI.difficulty+=1#increase the difficulty
-        if AI.difficulty==5:#if the difficulty is more than the max then reset it
-            AI.difficulty=0
-        setLED()#update the LED
+    #if GPIO.input(difficultyButtonPin)==1:#if the difficulty button is pressed
+    #    while GPIO.input(difficultyButtonPin)==1:
+    #        2+2#wait until the button is released
+    #    AI.difficulty+=1#increase the difficulty
+    #    if AI.difficulty==5:#if the difficulty is more than the max then reset it
+    #        AI.difficulty=0
 
     if GPIO.input(goButtonPin)==1:
         #insert CV board updating here
         tile = AI.botGo()#choose where to place the O
         setBasePos(baseServoPositions[tile])#rotate the robot to the correct position
-        armServo1.value=armServo1Positions[tile]#move the arm to that position
-        armServo2.value=armServo2Positions[tile]
-        armServo3.value=armServo3Positions[tile]
-        armServo4.value=armServo4Positions[tile]
-        armServo5.value=armServo5Positions[tile]
+        servos.servo[armServo1].angle=armServo1Positions[tile]#move the arm to that position
+        servos.servo[armServo2].angle=armServo2Positions[tile]
+        servos.servo[armServo3].angle=armServo3Positions[tile]
+        servos.servo[armServo4].angle=armServo4Positions[tile]
+        servos.servo[armServo5].angle=armServo5Positions[tile]
         time.sleep(0.5)#give the arm time to move there
-        handServo.value=0#open the hand
+        servos.servo[handServo]=90#open the hand
         time.sleep(0.25)#give it time
-        armServo1.value=0#reset the arm back to initial positions
-        armServo2.value=0
-        armServo3.value=0
-        armServo4.value=0
-        armServo5.value=0
+        servos.servo[armServo1].angle=armServo1Positions[-1]#reset the arm back to initial positions
+        servos.servo[armServo2].angle=armServo2Positions[-1]
+        servos.servo[armServo3].angle=armServo3Positions[-1]
+        servos.servo[armServo4].angle=armServo4Positions[-1]
+        servos.servo[armServo5].angle=armServo5Positions[-1]
         time.sleep(0.5)#give the arm time to move
-        setBasePos(initialBasePosition)#move the robot back to it original position
-        handServo.value=1#close the hand grabbing the next O
+        setBasePos(baseServoPositions[-1])#move the robot back to it original position
+        servos.servo[handServo]=1#close the hand grabbing the next o
         time.sleep(0.25)#give it time
--- servoConfigTest.py
+++ servoConfigTest.py
@@ -2,6 +2,9 @@ import Main
 
 vals=[0,0,0,0,0,0,0]
 def servoConfig():
+    global vals
+    vals=[Main.armServo1Positions[-1],Main.armServo2Positions[-1],Main.armServo3Positions[-1],Main.armServo4Positions[-1],Main.armServo5Positions[-1],Main.armServo6Positions[-1],Main.handServoPositions[-1]]
+    print("send a number between 0 and 6 press enter then send a number for the value to set a servo. or send ls to print current values or send s to apply the values to the servos")
     while True:
         inp=input("ready:")
         if inp.isnumeric():
@@ -11,10 +14,10 @@ def servoConfig():
         if inp=='ls':
             print(vals)
         if inp =='s':
-            Main.armServo1.value=vals[0]
-            Main.armServo2.value=vals[1]
-            Main.armServo3.value=vals[2]
-            Main.armServo4.value=vals[3]
-            Main.armServo5.value=vals[4]
-            Main.handServo.value=vals[5]
-            Main.baseServo.value=vals[6]
+            Main.servos.servo[Main.armServo1].angle=vals[0]
+            Main.servos.servo[Main.armServo2].angle=vals[1]
+            Main.servos.servo[Main.armServo3].angle=vals[2]
+            Main.servos.servo[Main.armServo4].angle=vals[3]
+            Main.servos.servo[Main.armServo5].angle=vals[4]
+            Main.servos.servo[Main.armServo6].angle=vals[5]
+            Main.servos.servo[Main.handServo].angle=vals[6]
