56ef40f62a27966c00e1bf2a3b9e613292948330
jSdCool <37940266+jSdCool@users.noreply.github.com>
2022-05-18 17:23:33 -0400

    e

--- servoConfigTest.py
+++ servoConfigTest.py
@@ -3,7 +3,7 @@
 vals=[0,0,0,0,0,0]
 def servoConfig(servos,armServo1,armServo2,armServo3,armServo4,armServo5,handServo):
     global vals
-    vals=[0,0,0,0,0,0]
+    vals=[90,90,90,90,90,90]
     print("send a number between 0 and 5 press enter then send a number for the value to set a servo. or send ls to print current values or send s to apply the values to the servos")
     print("0- wrist servo. 1-last joint before hand. 2- servo under the controller board. 3/4- servos at the base of the arm. 5-claw servo")
     while True:
@@ -20,4 +20,4 @@ def servoConfig(servos,armServo1,armServo2,armServo3,armServo4,armServo5,handSer
             servos.servo[armServo3].angle=vals[2]
             servos.servo[armServo4].angle=vals[3]
             servos.servo[armServo5].angle=vals[4]
-            servos.servo[handServo].angle=vals[6]
+            servos.servo[handServo].angle=vals[5]
